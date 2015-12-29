5889db6fc1167d58aa72f3e1193c2c0d7ed61cd0
Kazushi Takahashi <kazushi@rvm.jp>
2015-12-29 11:24:44 +0900

    change the design of the employee list

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,15 @@
 module EmployeesHelper
+
+  def get_tile_image(photoid)
+    PHOTO_CONFIG['thumbnail_small_dir'] + "/thumbnail_#{photoid}.jpg"
+  end
+
+  def get_recent_photo_path_for_bio(employee)
+    if employee.photos.last.present?
+      return photo_thumbnail_url(id: employee.photos.last.id, type: "small")
+    else
+      return "/images/no_image.png"
+    end
+  end
+
 end
