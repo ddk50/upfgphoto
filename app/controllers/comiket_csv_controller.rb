b19a5126361cee11ca59aa1c294237b74d8d3a84
Kazushi Takahashi <kazushi@rvm.jp>
2016-07-19 07:30:01 +0900

    add a comiket form

--- app/controllers/comiket_csv_controller.rb
+++ app/controllers/comiket_csv_controller.rb
@@ -29,6 +29,7 @@ class ComiketCsvController < ApplicationController
     rescue InvalidFileFormat => e
       redirect_to :back, alert: "#{e}"
     rescue => e
+      logger.fatal e.backtrace.join("\n")
       redirect_to :back, alert: "Error #{e}"
     end
   end
@@ -52,25 +53,117 @@ class ComiketCsvController < ApplicationController
   end
 
   private
+
+  CODES = {
+    NKF::JIS      => 'JIS',
+    NKF::EUC      => 'EUC',
+    NKF::SJIS     => 'SJIS',
+    NKF::UTF8       => 'UTF-8',
+    NKF::UTF16    => 'UTF-16',
+    NKF::BINARY   => 'BINARY',
+    NKF::ASCII    => 'ASCII',
+    NKF::UNKNOWN  => 'UNKNOWN'
+  }
+
+  ##
+  ## row[6]: 東
+  ## row[8]: 36
+  ## row[7]: A
+  ## row[10]: サークル名
+  ## row[21]: a/b
+  ##
   def convert_csv(filepath, tmpfilepath)
+
+    f = open(filepath, 'r')
+    contents = f.read
+    encode = CODES[NKF.guess(contents)]
+
+    logger.debug("*********************** #{encode} ************************")
+
+    f.close
+
+    case encode
+    when 'UTF-8'
+      save_csv_as(filepath, tmpfilepath, "UTF-8")
+    when 'SJIS'
+      save_csv_as(filepath, tmpfilepath, "Shift_JIS")
+    else
+      raise InvalidFileFormat, "不明な文字コードのCSVファイルです"
+    end
+  end
+  
+  def save_csv_as(filepath, outputpath, encode)
     intro_csv = CSV.generate do |csv|
-      CSV.foreach(filepath, headers: true, encoding: "Shift_JIS:UTF-8") do |row|
-        intro_msg = [
-                     row[5].encode("Shift_JIS"),
-                     row[6].encode("Shift_JIS"),
-                     row[7].encode("Shift_JIS"),
-                     row[8].encode("Shift_JIS"),
-                     row[10].encode("Shift_JIS"),
-                     row[12].nil? ? "".encode("Shift_JIS") : row[12].encode("Shift_JIS"),
-                     row[21].encode("Shift_JIS")
-                    ]
-        csv << intro_msg
+
+      csv << ["通し番号".encode("Shift_JIS"), 
+              "日付".encode("Shift_JIS"), 
+              "スペース".encode("Shift_JIS"), 
+              "サークル名".encode("Shift_JIS"), 
+              "アイテム".encode("Shift_JIS"), 
+              "単価".encode("Shift_JIS"), 
+              "発注数".encode("Shift_JIS"), 
+              "発注者".encode("Shift_JIS"), 
+              "備考".encode("Shift_JIS")]
+
+      open(filepath, "r:#{encode}", undef: :replace, invalid: :replace) do |f|
+        CSV.new(f).each do |row|
+          if row[0] =~ /Circle/
+            date = sanitize_date(row[5])
+            space = concat_spacenum(row[6], row[8], row[7], row[21]).encode("Shift_JIS", :undef => :replace)
+            intro_msg = [
+                         "".encode("Shift_JIS"),
+                         date.encode("Shift_JIS"),
+                         date == "欠席" ? "欠席".encode("Shift_JIS") : space,
+                         sanitize_circlename(row[10]).encode("Shift_JIS", :undef => :replace),
+                         "新刊".encode("Shift_JIS", :undef => :replace),
+                         "".encode("Shift_JIS", :undef => :replace),
+                         "1".encode("Shift_JIS"),
+                         current_employee.nickname,
+                         "".encode("Shift_JIS", :undef => :replace)
+                        ]
+            csv << intro_msg
+          end        
+        end
       end
     end
-
-    File.open(tmpfilepath, 'w:shift_jis') do |file|
+    File.open(outputpath, 'w:shift_jis') do |file|
       file.write(intro_csv)
-    end    
+    end
+  end
+
+  def sanitize_date(date)
+    case date
+    when "月"
+      return date
+    when "火"
+      return date
+    when "水"
+      return date
+    when "木"
+      return date
+    when "金"
+      return date
+    when "土"
+      return date
+    when "日"
+      return date
+    else
+      return "欠席"
+    end
+  end
+  
+  def concat_spacenum(row6, row8, row7, row21)
+    row6 = "" if not row6.present?
+    row7 = "" if not row7.present?
+    row8 = "" if not row8.present?
+    row21 = "" if not row21.present?
+    ab = row21 == "0" ? "a" : "b"
+    return row6 + row7 + row8 + ab
+  end
+
+  def sanitize_circlename(row10)
+    row10 = "" if not row10.present?
+    return row10
   end
   
 end
