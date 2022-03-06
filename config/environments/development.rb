b3a103e79854cad3cfc2460cc72373afbd39f922
kazushi takahashi <kazushi@rvm.jp>
2022-03-06 19:12:21 +0900

    ignores for MacOS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,15 +27,13 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  # Adds additional error checking when serving assets at runtime.
+  # Adds additional error checking whenÏ serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.cache_store = :dalli_store
 
   config.force_ssl = false
 end
